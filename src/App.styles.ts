84092b3da3b67dcda301fd4eb2423e7eb99b350a
Diego Rocha <dkxever@gmail.com>
2022-02-10 22:03:53 -0300

    Styling and structuring index App, and importing our PhotoGallery

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: var(--background-app);
+  min-height: 100vh;
+  color: #fff;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 30px 2%;
+`;
+
+export const Heading = styled.h1`
+  text-align: center;
+  font-size: var(--medium);
+  font-weight: bold;
+  margin-bottom: 30px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import * as C from './App.styles';
+import PhotoGallery from './Components/PhotoGallery';
+
+// Render App Final
+
 const App = () => {
-  return (
-    <div>
-      
-    </div>
+  return(
+    <C.Container>
+      <C.Wrapper>
+        <C.Heading>Galeria de Fotos</C.Heading>
+        {/* Componente de Galeria de Fotos */}
+        <PhotoGallery/>
+      </C.Wrapper>
+    </C.Container>
   )
 }
 
