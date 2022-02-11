fb23fcc7e62d4cb822e67229553a2e8c172371b1
Diego Rocha <dkxever@gmail.com>
2022-02-10 22:11:53 -0300

    Creating state hooks and loading verification with photos

--- src/Components/PhotoGallery/index.tsx
+++ src/Components/PhotoGallery/index.tsx
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
+import * as Photos from "../../services/photos";
+import { Photo } from "../../types/photo";
+
 const PhotoGallery = () => {
-  return(
-    <div></div>
+  
+  // Estados de loading e uploading para carregamento e envio de imagens
+
+  const [ loading, setLoading ] = useState(false);
+  const [ photos, setPhotos ] = useState<Photo[]>([]);
+
+  // Executa nossa função de listagem de imagens, sempre que iniciar a página ou quando for chamada.
+
+  useEffect(() => {
+    getPhotos();
+  },[]);
+
+  // Lista todas as imagens dentro do nosso firebase
+
+  const getPhotos = async () => {
+    setLoading(true);
+    setPhotos(await Photos.getAll());
+    setLoading(false);
+  }
+
+  return (
+    <>
+
+      {/* Verificação de loadings e estado de photos*/}
+
+      {loading && 
+        <C.ScreenWarning>
+          <div className="emoji">🐑</div>
+          <div>Carregando...</div>
+        </C.ScreenWarning>
+      }
+
+      {/* Componente PhotoItem que recebe dados de estado por mapping */}
+
+      {!loading && photos.length > 0 && 
+        <C.PhotoList>
+          {photos.map((item, index) => {
+            return(
+              <ul key={index}>
+                <li>{item.name}</li>
+              </ul>
+            )
+          })}
+        </C.PhotoList>
+      }
+
+      {!loading && photos.length === 0 &&
+        <C.ScreenWarning>
+          <div className="emoji">😞</div>
+          <div>Não há fotos cadastradas.</div>
+        </C.ScreenWarning>
+      }
+    </>
   )
+
 }
 
 export default PhotoGallery;
