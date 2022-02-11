ae907788a3f60096a748e14f20fb9b8662859f93
Diego Rocha <dkxever@gmail.com>
2022-02-10 22:31:40 -0300

    Styling the UploadForm form

--- src/Components/PhotoGallery/styles.ts
+++ src/Components/PhotoGallery/styles.ts
@@ -13,3 +13,26 @@ export const PhotoList = styled.div`
   gap: var(--small);
   grid-template-columns: repeat(3, minmax(250px, 1fr));
 `;
+
+export const UploadForm = styled.form`
+  background-color: #3d3040;
+  padding: var(--small);
+  border-radius: 0.5rem;
+  margin-bottom: 30px;
+
+  input[type="submit"]{
+    font-size: 14px;
+    padding: 0.3rem 1.5rem;
+    background-color: var(--default-btn);
+    color: #fff;
+    border: none;
+    border-radius: var(--small);
+    cursor: pointer;
+    margin: 0 25px;
+    transition: 0.3s;
+
+    &:hover{
+      background-color: var(--over-btn);
+    }
+  }
+`;
